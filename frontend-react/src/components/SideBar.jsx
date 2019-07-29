c48a48f94f0b308fdd9d3a7cfd21418f7ed28508
Tommi Lehtisaari <tommi@Tommi-MacBook-Pro.local>
2019-07-29 09:19:39 +0300

    installed report section to sidebar

--- frontend-react/src/components/SideBar.jsx
+++ frontend-react/src/components/SideBar.jsx
@@ -4,15 +4,7 @@ import { Icon, Menu, Sidebar, Image } from 'semantic-ui-react'
 
 const SideBar = ({ admin }) => (
   <div className="ts-nav-container">
-    <Sidebar
-      as={Menu}
-      icon="labeled"
-      animation="slide out"
-      inverted
-      vertical
-      visible
-      width="thin"
-    >
+    <Sidebar as={Menu} icon="labeled" animation="slide out" inverted vertical visible width="thin">
       <Menu.Item as="a">
         <Image src="logo.png" size="tiny" centered />
       </Menu.Item>
@@ -25,10 +17,16 @@ const SideBar = ({ admin }) => (
         Projects
       </Menu.Item>
       {admin && (
-        <Menu.Item as={NavLink} to="/users">
-          <Icon name="user circle" />
-          Users
-        </Menu.Item>
+        <React.Fragment>
+          <Menu.Item as={NavLink} to="/users">
+            <Icon name="user circle" />
+            Users
+          </Menu.Item>
+          <Menu.Item as={NavLink} to="/report">
+            <Icon name="chart bar outline" />
+            Report
+          </Menu.Item>
+        </React.Fragment>
       )}
       <Menu.Item as={NavLink} to="/logout">
         <Icon name="sign-out" />
